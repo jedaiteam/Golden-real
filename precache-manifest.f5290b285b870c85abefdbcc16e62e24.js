9db52f54513c362bddc1af9c7a6d428e44b8fdc3
jedaiteam <fakhri.ahmadov@jedai.az>
2021-10-15 16:52:42 +0400

    Updates

---
+++ precache-manifest.f5290b285b870c85abefdbcc16e62e24.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bb7c8cab699a935e43f17071e303a6",
+    "url": "/Golden_Light/index.html"
+  },
+  {
+    "revision": "85cf064cd9b851a1c717",
+    "url": "/Golden_Light/static/css/2.c71ed755.chunk.css"
+  },
+  {
+    "revision": "202c7f6d4de0f11bed75",
+    "url": "/Golden_Light/static/css/main.67a8f5a6.chunk.css"
+  },
+  {
+    "revision": "85cf064cd9b851a1c717",
+    "url": "/Golden_Light/static/js/2.8028e500.chunk.js"
+  },
+  {
+    "revision": "202c7f6d4de0f11bed75",
+    "url": "/Golden_Light/static/js/main.a7c180ef.chunk.js"
+  },
+  {
+    "revision": "68fa35784cbf36c45f19",
+    "url": "/Golden_Light/static/js/runtime~main.0e5b513f.js"
+  },
+  {
+    "revision": "6ef6709d82148505ead85625e18fb45e",
+    "url": "/Golden_Light/static/media/1517602717572.6ef6709d.jfif"
+  },
+  {
+    "revision": "4f3ea1da37fee545a8ba82defaf56a87",
+    "url": "/Golden_Light/static/media/2.4f3ea1da.jpg"
+  },
+  {
+    "revision": "4dda12d453fa1bb8e7ce4d3dfd477c34",
+    "url": "/Golden_Light/static/media/FuturaPT-Demi.4dda12d4.woff"
+  },
+  {
+    "revision": "6c10b6a0a4425405d29b8b08b4169bda",
+    "url": "/Golden_Light/static/media/FuturaPT-Demi.6c10b6a0.woff2"
+  },
+  {
+    "revision": "1ecc005d94c761ba020d44c564a77634",
+    "url": "/Golden_Light/static/media/FuturaPT-DemiObl.1ecc005d.woff"
+  },
+  {
+    "revision": "d2264cc74bfe704a9ff302d4000baf96",
+    "url": "/Golden_Light/static/media/FuturaPT-DemiObl.d2264cc7.woff2"
+  },
+  {
+    "revision": "5085eff964cd2716d8f605a6e0dc3209",
+    "url": "/Golden_Light/static/media/FuturaPT-Heavy.5085eff9.woff2"
+  },
+  {
+    "revision": "95247b1c95026d66963e6afffc45bfda",
+    "url": "/Golden_Light/static/media/FuturaPT-Heavy.95247b1c.woff"
+  },
+  {
+    "revision": "2b37dd100f7bbd52eaf390526352dace",
+    "url": "/Golden_Light/static/media/FuturaPT-HeavyObl.2b37dd10.woff2"
+  },
+  {
+    "revision": "5d6ae1281780870b691ed90b6ae4b5ee",
+    "url": "/Golden_Light/static/media/FuturaPT-HeavyObl.5d6ae128.woff"
+  },
+  {
+    "revision": "05acf1f00bc4c350d28c9ee238a548ec",
+    "url": "/Golden_Light/static/media/FuturaPT-Light.05acf1f0.woff2"
+  },
+  {
+    "revision": "c099b2320c2ec69c6f8d24fe3648a7f8",
+    "url": "/Golden_Light/static/media/FuturaPT-Light.c099b232.woff"
+  },
+  {
+    "revision": "17fe1dfffb41ae8dd8b9fb8e3f39d395",
+    "url": "/Golden_Light/static/media/FuturaPT-LightObl.17fe1dff.woff"
+  },
+  {
+    "revision": "fdaf8eb49bfe0ee89f6d285e6f89526c",
+    "url": "/Golden_Light/static/media/FuturaPT-LightObl.fdaf8eb4.woff2"
+  },
+  {
+    "revision": "a73a85bb875d4c7519057fc4debefff2",
+    "url": "/Golden_Light/static/media/FuturaPT-Medium.a73a85bb.woff2"
+  },
+  {
+    "revision": "cdfeadb397f99d7afcedb8d518b66aaa",
+    "url": "/Golden_Light/static/media/FuturaPT-Medium.cdfeadb3.woff"
+  },
+  {
+    "revision": "7ae7ad9dccce477f5dc82fd87d098638",
+    "url": "/Golden_Light/static/media/FuturaPT-MediumObl.7ae7ad9d.woff2"
+  },
+  {
+    "revision": "a373f99c071d96eaf44dfcbf71289e59",
+    "url": "/Golden_Light/static/media/FuturaPT-MediumObl.a373f99c.woff"
+  },
+  {
+    "revision": "2390efe63f4cbd6ae08de6fc915f2644",
+    "url": "/Golden_Light/static/media/ab-shape-1.2390efe6.png"
+  },
+  {
+    "revision": "8e1a4cdd357983bd97d62e33cd80d5e5",
+    "url": "/Golden_Light/static/media/ab.8e1a4cdd.png"
+  },
+  {
+    "revision": "97f754ae581aa6223f3944dda2ced529",
+    "url": "/Golden_Light/static/media/badge.97f754ae.svg"
+  },
+  {
+    "revision": "75e8d96e6870ffc827172b99fb099a4a",
+    "url": "/Golden_Light/static/media/bar-graph.75e8d96e.svg"
+  },
+  {
+    "revision": "5b1975080d5f3932ebe09d69f31dcecd",
+    "url": "/Golden_Light/static/media/briefcase.5b197508.svg"
+  },
+  {
+    "revision": "00b6eab1cc40f0e4742b5de20ae717e9",
+    "url": "/Golden_Light/static/media/chart.00b6eab1.svg"
+  },
+  {
+    "revision": "0cef2a234a6084ed360a66689da58093",
+    "url": "/Golden_Light/static/media/clipboard.0cef2a23.svg"
+  },
+  {
+    "revision": "8a4de5f99c5e4f8c0b9e8c2ebe79ff3a",
+    "url": "/Golden_Light/static/media/document.8a4de5f9.svg"
+  },
+  {
+    "revision": "2096571074feb5eeacf54f460952420f",
+    "url": "/Golden_Light/static/media/flaticon.20965710.woff"
+  },
+  {
+    "revision": "6d1fe0e5ddd2064d1a080259e881f5a9",
+    "url": "/Golden_Light/static/media/flaticon.6d1fe0e5.ttf"
+  },
+  {
+    "revision": "75713cd0fe92267c82649658f1a3b271",
+    "url": "/Golden_Light/static/media/flaticon.75713cd0.svg"
+  },
+  {
+    "revision": "a026f854e108ff2054e42056cdaadf23",
+    "url": "/Golden_Light/static/media/flaticon.a026f854.woff2"
+  },
+  {
+    "revision": "b2b2b8365448ee0be6c05fa34eb7d77b",
+    "url": "/Golden_Light/static/media/flaticon.b2b2b836.eot"
+  },
+  {
+    "revision": "04b71bec9cef872e26ce7bc00b9dd0cc",
+    "url": "/Golden_Light/static/media/fontawesome-webfont.04b71bec.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Golden_Light/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Golden_Light/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Golden_Light/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Golden_Light/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d688863102636525449b1e705b96c2f4",
+    "url": "/Golden_Light/static/media/img-1.d6888631.jfif"
+  },
+  {
+    "revision": "c7b77face4002e7806392b0a08f2b3ae",
+    "url": "/Golden_Light/static/media/img-2.c7b77fac.jfif"
+  },
+  {
+    "revision": "db4b6916101bda388c4a269c1d8bfcbb",
+    "url": "/Golden_Light/static/media/img-3.db4b6916.jfif"
+  },
+  {
+    "revision": "1836ea1591f45eba012c8a33ad893b43",
+    "url": "/Golden_Light/static/media/img3.1836ea15.jpeg"
+  },
+  {
+    "revision": "8ad7ca8731c27b15f109b82bbab4302d",
+    "url": "/Golden_Light/static/media/logo-main.8ad7ca87.png"
+  },
+  {
+    "revision": "8ad7ca8731c27b15f109b82bbab4302d",
+    "url": "/Golden_Light/static/media/logo22.8ad7ca87.png"
+  },
+  {
+    "revision": "812dcd9e5e797a1ef71491f358dc2c26",
+    "url": "/Golden_Light/static/media/medical1.812dcd9e.svg"
+  },
+  {
+    "revision": "bf514309c4d980addc880f9c162dc559",
+    "url": "/Golden_Light/static/media/page-title.bf514309.jpg"
+  },
+  {
+    "revision": "a1c620a5a255eee0b4b0b9e97da6fa7a",
+    "url": "/Golden_Light/static/media/right.a1c620a5.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/Golden_Light/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/Golden_Light/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/Golden_Light/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/Golden_Light/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d424184cc0b5398b3a96f1f4056d79fd",
+    "url": "/Golden_Light/static/media/slide-2.d424184c.jpg"
+  },
+  {
+    "revision": "d424184cc0b5398b3a96f1f4056d79fd",
+    "url": "/Golden_Light/static/media/slide-3.d424184c.jpg"
+  },
+  {
+    "revision": "2c454669bdf3aebf32a1bd8ac1e0d2d6",
+    "url": "/Golden_Light/static/media/themify.2c454669.eot"
+  },
+  {
+    "revision": "9c8e96ecc7fa01e6ebcd196495ed2db5",
+    "url": "/Golden_Light/static/media/themify.9c8e96ec.svg"
+  },
+  {
+    "revision": "a1ecc3b826d01251edddf29c3e4e1e97",
+    "url": "/Golden_Light/static/media/themify.a1ecc3b8.woff"
+  },
+  {
+    "revision": "e23a7dcaefbde4e74e263247aa42ecd7",
+    "url": "/Golden_Light/static/media/themify.e23a7dca.ttf"
+  }
+]);
